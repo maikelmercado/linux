cfbecd0493edce9f2c5f64047783eafcc0aa34b8
maik <maikmercadom@gmail.com>
2021-03-07 19:05:35 -0300

    desde el mac domingo

---
+++ modificadores/modificadores.ts
@@ -0,0 +1,23 @@
+//modificadores privado es accesible solo dentro de la clase 
+//modificador publico puede ser de cualquier lado
+
+
+class Animal{
+
+    private nombre:string;
+    private tamaño:number;
+
+
+    constructor (nombre_:string, tamaño_:number){
+
+        this.nombre=nombre_;
+        this.tamaño=tamaño_;
+    }
+
+    private correr():void{
+
+        console.log("correr fuerte");
+    }
+
+}
+const perro = new Animal('linco', 23);
